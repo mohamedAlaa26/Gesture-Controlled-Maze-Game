95ade372f88043d4437ad3b72903cfca907d3bae
mohamedAlaa26 <2001mohamed.alaa26@gmail.com>
2025-05-31 11:55:08 +0300

    added the docker-compose files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.predict.logic import predict_label
 from app.schemas.input import LandmarkInput
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
 
 @app.post("/predict")
 def predict(data: LandmarkInput):
